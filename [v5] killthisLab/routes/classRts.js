29efb6274028baedf24dc2d2094a2be2fcffa152
Ashcorpuz <137604451+Ashcorpuz@users.noreply.github.com>
2024-07-12 02:21:57 +0800

    Delete [v5] killthisLab directory

--- [v5] killthisLab/routes/classRts.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const classController = require('../controller/classCtrl');
-
-router.get('/classes', classController.getAllClass);
-module.exports = router;
