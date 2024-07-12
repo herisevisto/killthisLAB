c97fa159d9fbc0a32c531ccc5f4e957b8d057f3a
Ashcorpuz <137604451+Ashcorpuz@users.noreply.github.com>
2024-07-12 22:51:28 +0800

    Delete killthisLab directory

--- killthisLab/routes/locationRts.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationController = require('../controller/locationCtrl');
-
-router.get('/locations', locationController.getAllLocations);;
-
-module.exports = router;
