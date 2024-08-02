f0a71fdf3665ed5d75e9d019644976f6725c17c8
Ashcorpuz <137604451+Ashcorpuz@users.noreply.github.com>
2024-08-02 23:02:26 +0800

    Delete CCAPDEV-MVC directory

--- CCAPDEV-MVC/controller/routes/locationRts.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const locationController = require('../locationCtrl');
-
-router.get('/locations', locationController.getAllLocations);;
-
-module.exports = router;
