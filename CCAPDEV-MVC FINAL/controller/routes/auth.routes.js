de5e0af2ee2c621715d7e063af68d48d180d4e7c
herisevisto <137604880+herisevisto@users.noreply.github.com>
2024-08-02 23:22:52 +0800

    Delete CCAPDEV-MVC FINAL directory

--- CCAPDEV-MVC FINAL/controller/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require("../auth.controller");
-
-router.post("/login", authController.login);
-router.post("/loginlabtech", authController.loginlabtech);
-router.post("/register", authController.register);
-router.get("/logout", authController.logout);
-router.post("/change-password", authController.changePassword);
-module.exports = router;
