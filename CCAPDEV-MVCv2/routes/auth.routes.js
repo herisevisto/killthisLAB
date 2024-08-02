d0a08115a18192d3eff8fb84173d1127aa0553f1
Ashcorpuz <137604451+Ashcorpuz@users.noreply.github.com>
2024-08-02 23:02:36 +0800

    Delete CCAPDEV-MVCv2 directory

--- CCAPDEV-MVCv2/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const authController = require("../controller/auth.controller");
-
-router.post("/login", authController.login);
-router.post("/loginlabtech", authController.loginlabtech);
-router.post("/register", authController.register);
-router.get("/logout", authController.logout);
-
-module.exports = router;
